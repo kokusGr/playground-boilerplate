4a1de3a7788bc822e27c7d150cb34cb1b0ce2598
Konrad <gren.konrad@gmail.com>
2018-01-11 06:41:33 +0100

    Add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
+  ]
+}
+
+module.exports = config
