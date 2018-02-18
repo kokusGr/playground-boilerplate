45ed91bbc963ccb850e2062987847f762547148d
Kokus <gren.konrad@gmail.com>
2018-02-18 11:44:28 +0100

    Add flow with basic config

--- app/index.js
+++ app/index.js
@@ -1,9 +1,15 @@
+// @flow
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Root from 'components/Root'
 
-ReactDOM.render(
-  <Root />,
-  document.getElementById('application'),
-)
+const container = document.getElementById('application')
+
+if (container) {
+  ReactDOM.render(
+    <Root />,
+    container,
+  )
+}
